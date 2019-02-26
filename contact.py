c0aa944e9bd51044abd66712d6bae302ca894c48
Vikil Naik <vikilnaik37@gmail.com>
2019-02-26 14:11:44 -0500

    Created a .gitignore file

--- contact.py
+++ contact.py
@@ -2,19 +2,19 @@ from peewee import SqliteDatabase, Model, CharField, TextField
 
 db = SqliteDatabase('crm.db')
 
+
 class Contact(Model):
     first_name = CharField()
     last_name = CharField()
     email = CharField()
     note = TextField()
 
+    class Meta:
+        database = db  # db is the SqliteDatabase instance we made above
 
-  class Meta:
-    database = db # db is the SqliteDatabase instance we made above
-
-    def full_name(self):
-    """Returns the full (first and last) name of the contact"""
-        return self.first_name + " " + self.last_name
+        def full_name(self):
+            """Returns the full (first and last) name of the contact"""
+            return self.first_name + " " + self.last_name
 
 
 # this goes outside the class
